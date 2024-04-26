11ed4d228efb53966b2578a958dd3067ed076f01
DanielCarrenoMar <dacarreno.24@est.ucab.edu.ve>
2024-04-26 13:06:51 -0400

    Refactor: Mover bucle de jeugo a pantalla Game

--- main.py
+++ main.py
@@ -10,7 +10,6 @@ from pages.Game import Game
 N = 21
 M = 12
 board = np.zeros((N, M), dtype=int)
-
 pieces = [Piece(board, shape_I, 1),Piece(board, shape_L, 3),Piece(board, shape_LI, 4),Piece(board, shape_D, 5), Piece(board, shape_O, 7, False), Piece(board, shape_T, 8)]
 
 pg.init()
@@ -25,60 +24,10 @@ time = 100
 timeT = 0
 move = [0,0]
 
-pages = [Game(board, inGame)]
+pages = [Game(clock,board, pieces)]
 page = 0
 
 while True:
-    deltaTime = clock.tick(60) / 1000
-    for event in pg.event.get():
-        if event.type == pg.QUIT:
-            pg.quit()
-            quit()
-        if event.type == pg.VIDEORESIZE:
-            #Reside screen
-            pass
-        if event.type == pg.KEYDOWN:
-            if event.key == pg.K_LEFT or event.key == pg.K_a:
-                inGame[0].move([-1,0])
-                move = [-1,0]
-                timeT = 0
-            if event.key == pg.K_RIGHT or event.key == pg.K_d:
-                inGame[0].move([1,0])
-                move = [1,0]
-                timeT = 0
-            if event.key == pg.K_DOWN or event.key == pg.K_s:
-                while not inGame[0].static:
-                    inGame[0].move([0,1])
-            if event.key == pg.K_UP or event.key == pg.K_w:
-                inGame[0].rotateR()
-        if event.type == pg.KEYUP:
-            move = [0,0]
-
-    if timeT > int(540 * deltaTime) and move != [0,0]: 
-        timeT = 0
-        inGame[0].move(move)
-
-    if time > int(1500 * deltaTime):
-        time = 0
-        if inGame[0].static: 
-            gamePieces.append(inGame.pop(0))
-            inGame.append(copy(choice(pieces)))
-
-        #Cambiar por recursividad y mandar señal a interfaz cuando se compelta una fila
-        for Y in range(3,N):
-            completeLine = True
-            for X in range(M):
-                if board[Y,X] == 0:
-                    completeLine = False
-
-            if completeLine: 
-                board[Y] = np.zeros(M, dtype=int)
-                board[3:Y+1] = np.roll(board[3:Y+1], shift=1, axis=0)
-
-
-        inGame[0].move([0,1])
-
     pages[page].bucle()
-    time += 1
-    timeT += 1
+    
     pg.display.flip()
--- pages/Game.py
+++ pages/Game.py
@@ -1,34 +1,100 @@
 import pygame as pg
 import numpy as np
 from Piece import Piece
+from random import choice
+from copy import copy
 
 colors = [(50,50,50), (100, 100, 200), (200, 100, 100), (100, 200, 100), (200, 200, 100), (100, 100, 200), (200, 100, 200), (100, 200, 200), (200, 200, 200)]
 
 class Game():
-    def __init__(self, board:np.ndarray[any], pieceList:list[Piece]):
+    def __init__(self, clock:pg.time.Clock, board:np.ndarray[any], pieces:list[Piece]):
+        self.clock = clock
         self.screen = pg.display.get_surface()
         self.board = board
-        self.pieceList = pieceList
+        self.pieces = pieces
+        self.pieceInGame = [copy(choice(pieces)) for _ in range(2)]
+        self.gamePieces = []
 
         self.dimY = self.board.shape[0]
         self.dimX = self.board.shape[1]
 
-    def bucle(self):
-        screen = self.screen
-        board = self.board
-        screen.fill((0,0,0))
+        self.time = 0
+        self.timeT = 0
+        self.move = [0,0]
+    def events(self):
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                pg.quit()
+                quit()
+            if event.type == pg.VIDEORESIZE:
+                #Reside screen
+                pass
+            if event.type == pg.KEYDOWN:
+                if event.key == pg.K_LEFT or event.key == pg.K_a:
+                    self.pieceInGame[0].move([-1,0])
+                    self.move = [-1,0]
+                    self.timeT = 0
+                if event.key == pg.K_RIGHT or event.key == pg.K_d:
+                    self.pieceInGame[0].move([1,0])
+                    self.move = [1,0]
+                    self.timeT = 0
+                if event.key == pg.K_DOWN or event.key == pg.K_s:
+                    while not self.pieceInGame[0].static:
+                        self.pieceInGame[0].move([0,1])
+                if event.key == pg.K_UP or event.key == pg.K_w:
+                    self.pieceInGame[0].rotateR()
+            if event.type == pg.KEYUP:
+                self.move = [0,0]
 
-        pg.draw.rect(screen, (255,255,255), (0, 0, self.dimX *30, (self.dimY-3) * 30))
+    def frontEnd(self):
+        self.screen.fill((0,0,0))
+
+        pg.draw.rect(self.screen, (255,255,255), (0, 0, self.dimX *30, (self.dimY-3) * 30))
 
         for Y in range(3, self.dimY):
             for X in range(self.dimX):
-                if board[Y,X] != 0:
-                    pg.draw.rect(screen, colors[board[Y,X]], (X * 30, (Y-3) * 30, 30, 30))
-                elif self.pieceList[0].x in [X,X-1,X-2]:
-                    pg.draw.rect(screen, (240,240,240), (X * 30, (Y-3) * 30, 30, 30))
+                if self.board[Y,X] != 0:
+                    pg.draw.rect(self.screen, colors[self.board[Y,X]], (X * 30, (Y-3) * 30, 30, 30))
+                elif self.pieceInGame[0].x in [X,X-1,X-2]:
+                    pg.draw.rect(self.screen, (240,240,240), (X * 30, (Y-3) * 30, 30, 30))
 
         for Y in range(3):
             for X in range(3):
-                if self.pieceList[1].shape[Y,X] != 0:
-                    pg.draw.rect(screen, colors[self.pieceList[1].value], (400 + X * 30, Y * 30, 30, 30))
+                if self.pieceInGame[1].shape[Y,X] != 0:
+                    pg.draw.rect(self.screen, colors[self.pieceInGame[1].value], (400 + X * 30, Y * 30, 30, 30))
+
+    def backEnd(self):
+        deltaTime = self.clock.tick(60) / 1000
+        if self.timeT > int(540 * deltaTime) and self.move != [0,0]: 
+            self.timeT = 0
+            self.pieceInGame[0].move(self.move)
+
+        if self.time > int(1500 * deltaTime):
+            self.time = 0
+            if self.pieceInGame[0].static: 
+                self.gamePieces.append(self.pieceInGame.pop(0))
+                self.pieceInGame.append(copy(choice(self.pieces)))
+
+            #Cambiar por recursividad
+            for Y in range(3,self.dimY):
+                completeLine = True
+                for X in range(self.dimX):
+                    if self.board[Y,X] == 0:
+                        completeLine = False
+
+                if completeLine: 
+                    self.board[Y] = np.zeros(self.dimX, dtype=int)
+                    self.board[3:Y+1] = np.roll(self.board[3:Y+1], shift=1, axis=0)
+
+            self.pieceInGame[0].move([0,1])
+
+        self.time += 1
+        self.timeT += 1
+
+    def bucle(self):
+        self.events()
+
+        self.frontEnd()
+
+        self.backEnd()
                     
