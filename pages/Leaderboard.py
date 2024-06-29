357bbf167eaaa763ac4e257667c4bf04e9cadc2e
DanielCarrenoMar <dacarreno.24@est.ucab.edu.ve>
2024-06-28 21:13:34 -0400

    Feat: Filtros mejorados, filtrar por usuario y estado

--- pages/Leaderboard.py
+++ pages/Leaderboard.py
@@ -13,9 +13,10 @@ class Leaderboard():
         self.H = pg.display.Info().current_h
         self.manager = pgu.UIManager((self.W,self.H))
 
-        self.isLoad = False
-        self.showState = "Bolívar"
+        self.showState = None
+        self.showUser = None
         self.textRenderDataLeader:list[pg.font.Font] = []
+        self.updateLeaderboard()
 
         self.buttonBack = pgu.elements.UIButton(
             relative_rect=pg.Rect((300, 50), (150, 50)),
@@ -24,7 +25,7 @@ class Leaderboard():
 
         self.inputState = pgu.elements.UIDropDownMenu(
             relative_rect=pg.Rect((250, 100), (500, 30)),
-            starting_option="Bolívar",
+            starting_option=("Ninguno", None),
             options_list=gv.states,
             manager=self.manager)
 
@@ -38,39 +39,49 @@ class Leaderboard():
                 pass
             if event.type == pgu.UI_BUTTON_PRESSED and event.ui_element == self.buttonBack:
                 gv.actualPage = 2
-                self.isLoad = False
             if event.type == pgu.UI_DROP_DOWN_MENU_CHANGED and event.ui_element == self.inputState:
-                self.isLoad = False
                 self.showState = self.inputState.selected_option[1]
+                self.updateLeaderboard()
 
             self.manager.process_events(event)
-    def showLeaderboard(self, estado:str = None, fecha:list[int] = None	):
-        if not self.isLoad:
-            self.textRenderDataLeader = []
-            for user in getAllUsers():
-                for score in user[4]:
-                    text = f"{user[3]} - {user[2]} Puntos: {score[0]} fecha: {score[1]}/{score[2]}/{score[3]} Hora: {score[4]}:{score[5]}"
-                    if estado == None and fecha == None:
-                        self.textRenderDataLeader.append(pg.font.Font(gv.fontLekton, 32).render(text, True, (255,255,255)))
-                    elif estado == None:
-                        pass
-                    elif fecha == None:
-                        if user[3] == estado:
-                            self.textRenderDataLeader.append(pg.font.Font(gv.fontLekton, 32).render(text, True, (255,255,255)))
-                    else:
-                        pass
+    
+    def updateLeaderboard(self):
+        self.textRenderDataLeader = []
+        scores = self.filterState(getAllUsers(),self.showState)
+        scores = self.filterUser(scores, self.showUser)
 
-            self.isLoad = True
-        
-        for index, text in enumerate(self.textRenderDataLeader):
-            self.screen.blit(text, (self.W//2 - text.get_width()//2, (50*index)+150))
+        for user in scores:
+            for score in user[4]:
+                text = f"{user[3]} - {user[2]} Puntos: {score[0]} fecha: {score[1]}/{score[2]}/{score[3]} Hora: {score[4]}:{score[5]}"
+                self.textRenderDataLeader.append(pg.font.Font(gv.fontLekton, 32).render(text, True, (255,255,255)))
+
+    def filterState(self, users:list[list],state:str):
+        if state == None: return users
+
+        newList = []
+        for user in users:
+            stateUser = user[3]
+            if stateUser == state:
+                newList.append(user)
+        return newList
+
+    def filterUser(self, users:list[list], mail:str):
+        if mail == None: return users
+
+        newList = []
+        for user in users:
+            mailUser = user[0]
+            if mailUser == mail:
+                newList.append(user)
+        return newList
 
     def frontEnd(self):
         self.screen.fill((0,0,0))
         self.manager.update(self.clock.tick(60)/1000)
         self.manager.draw_ui(self.screen)
 
-        self.showLeaderboard(estado=self.showState)
+        for index, text in enumerate(self.textRenderDataLeader):
+            self.screen.blit(text, (self.W//2 - text.get_width()//2, (50*index)+150))
 
     def backEnd(self):
         pass
