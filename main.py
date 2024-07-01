5e6bba843a67f75794b58103b8ef5a74350dd647
DanielCarrenoMar <dacarreno.24@est.ucab.edu.ve>
2024-06-30 20:58:28 -0400

    Feat: Nombre y Icono

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import pygame as pg
 
 import globalVariables as gv
 import public.sonds.loadSonds as sonds
+import public.images.loadImages as img
 
 from pages.Login import Login
 from pages.Register import Register
@@ -22,6 +23,8 @@ pg.init()
 W = pg.display.Info().current_w
 H = pg.display.Info().current_h
 pg.display.set_mode((W-100, H-100), pg.RESIZABLE)
+pg.display.set_caption("Tetris")
+pg.display.set_icon(img.pieces["blue"])
 clock = pg.time.Clock()
 
 try:
