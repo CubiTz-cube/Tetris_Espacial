fd9efd32784971c74e23e1f68b73018b410a7fea
DanielCarrenoMar <dacarreno.24@est.ucab.edu.ve>
2024-07-05 14:37:42 -0400

    Refactor: poner dynamic objects en selections

--- pages/Selection.py
+++ pages/Selection.py
@@ -4,6 +4,7 @@ from pygame_gui.core import ObjectID
 
 import globalVariables as gv
 import public.images.loadImages as img
+from library.dynamicObjects import *
 from library.piece import allPieces, imgCompletePiecesNum
 from library.imageEdit import convertImgToBn
 
@@ -16,49 +17,34 @@ class Selection():
         self.manager = pgu.UIManager((self.W,self.H), "pages\\css\\selection.json")
         self.manager.get_theme().load_theme("pages\\css\\global.json")
 
-        self.buttonPlay = pgu.elements.UIButton(
-        relative_rect=pg.Rect((300, 50), (150, 50)),
-        text="Iniciar",
-        manager=self.manager)
+        self.buttonPlay = DynamicButton(300, 50, 150, 50, "Iniciar", self.manager)
+        self.buttonBack = DynamicButton(100, 50, 150, 50, "Regresar", self.manager)
+        self.inputMode = DynamicDropDown(450, 150, 150, 30, self.manager, ["Desactivado", "Tiempo", "Pieza"], "Desactivado")
+        self.inputLimit = DynamicInput(300, 150, 150, 30, self.manager, str(gv.limit))
+        self.inputDimX = DynamicInput(350, 200, 100, 30, self.manager, str(gv.dimX))
+        self.inputDimY = DynamicInput(250, 200, 100, 30, self.manager, str(gv.dimY))
 
-        self.buttonBack = pgu.elements.UIButton(
-        relative_rect=pg.Rect((100, 50), (150, 50)),
-        text="Regresar",
-        manager=self.manager)
-
-        self.inputMode = pgu.elements.UIDropDownMenu(
-        relative_rect=pg.Rect((300, 100), (150, 30)),
-        starting_option="Desactivado",
-        options_list=["Desactivado", "Tiempo", "Pieza"],
-        manager=self.manager)
-
-        self.inputLimit = pgu.elements.UITextEntryLine(
-        relative_rect=pg.Rect((300, 150), (150, 30)),
-        initial_text="0",
-        manager=self.manager)
-
-        self.inputDimX = pgu.elements.UITextEntryLine(
-        relative_rect=pg.Rect((350, 200), (100, 30)),
-        initial_text="12",
-        manager=self.manager)
-
-        self.inputDimY = pgu.elements.UITextEntryLine(
-        relative_rect=pg.Rect((250, 200), (100, 30)),
-        initial_text="21",
-        manager=self.manager)
+        self.dinamicObjects = [
+            self.buttonPlay,
+            self.buttonBack, 
+            self.inputMode, 
+            self.inputLimit, 
+            self.inputDimX, 
+            self.inputDimY
+        ]
 
         self.pieceButtons:list[pgu.elements.UIButton] = []
         self.pieceImages:list[pgu.elements.UIImage] = []
         for i in range(len(imgCompletePiecesNum)):
             imageSize = imgCompletePiecesNum[i].get_size()
             self.pieceButtons.append(pgu.elements.UIButton(
-            relative_rect=pg.Rect((100 + 100 * i, 250), (imageSize[0]//2,imageSize[1]//2)),
+            relative_rect=pg.Rect((100 + 100 * i, 300), (imageSize[0]//2,imageSize[1]//2)),
             text="",
             manager=self.manager,
             object_id= ObjectID("#select","@transparent"),
             ))
             self.pieceImages.append(pgu.elements.UIImage(
-            relative_rect=pg.Rect((100 + 100 * i, 250), (imageSize[0]//2,imageSize[1]//2)),
+            relative_rect=pg.Rect((100 + 100 * i, 300), (imageSize[0]//2,imageSize[1]//2)),
             image_surface=imgCompletePiecesNum[i],
             manager=self.manager))
 
@@ -68,35 +54,34 @@ class Selection():
                 pg.quit()
                 quit()
             if event.type == pg.VIDEORESIZE:
-                #Reside screen
-                pass
-            if event.type == pgu.UI_BUTTON_PRESSED and event.ui_element == self.buttonPlay:
+                self.resize()
+            if event.type == pgu.UI_BUTTON_PRESSED and event.ui_element == self.buttonPlay.element:
                 gv.actualPage = 4
-            if event.type == pgu.UI_BUTTON_PRESSED and event.ui_element == self.buttonBack:
+            if event.type == pgu.UI_BUTTON_PRESSED and event.ui_element == self.buttonBack.element:
                 gv.actualPage = 2
-            if event.type == pgu.UI_DROP_DOWN_MENU_CHANGED and event.ui_element == self.inputMode:
-                if self.inputMode.selected_option[0] == "Desactivado": 
+            if event.type == pgu.UI_DROP_DOWN_MENU_CHANGED and event.ui_element == self.inputMode.element:
+                if self.inputMode.element.selected_option[0] == "Desactivado": 
                     gv.limit = 0
                     gv.mode = 0
-                if self.inputMode.selected_option[0] == "Tiempo": 
+                if self.inputMode.element.selected_option[0] == "Tiempo": 
                     gv.mode = 1
-                if self.inputMode.selected_option[0] == "Pieza": 
+                if self.inputMode.element.selected_option[0] == "Pieza": 
                     gv.mode = 2
-            if event.type == pgu.UI_TEXT_ENTRY_CHANGED and event.ui_element == self.inputLimit:
+            if event.type == pgu.UI_TEXT_ENTRY_CHANGED and event.ui_element == self.inputLimit.element:
                 try:
-                    gv.limit = int(self.inputLimit.get_text())
+                    gv.limit = int(self.inputLimit.element.get_text())
                 except:
                     pass
-            if event.type == pgu.UI_TEXT_ENTRY_CHANGED and event.ui_element == self.inputDimX:
+            if event.type == pgu.UI_TEXT_ENTRY_CHANGED and event.ui_element == self.inputDimX.element:
                 try:
-                    change = int(self.inputDimX.get_text())
+                    change = int(self.inputDimX.element.get_text())
                     if change % 3 == 0 and change > 9: gv.dimX = change
                     else: print("dimX no es multiplo de 3 mayor que 9")
                 except:
                     pass
-            if event.type == pgu.UI_TEXT_ENTRY_CHANGED and event.ui_element == self.inputDimY:
+            if event.type == pgu.UI_TEXT_ENTRY_CHANGED and event.ui_element == self.inputDimY.element:
                 try:
-                    change = int(self.inputDimY.get_text())
+                    change = int(self.inputDimY.element.get_text())
                     if change % 3 == 0 and change > 9: gv.dimX = change
                     else: print("dimY no es multiplo de 3 mayor que 9")
                 except:
@@ -114,6 +99,10 @@ class Selection():
                         self.pieceImages[index].set_image(convertImgToBn(imgCompletePiecesNum[index]))
 
             self.manager.process_events(event)
+
+    def resize(self):
+        for obj in self.dinamicObjects:
+            obj.resize()
 
     def frontEnd(self):
         self.screen.fill("#050611")
